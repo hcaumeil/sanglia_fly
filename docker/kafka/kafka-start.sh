be9e14a498b69c26859d85849efe13879ea9c510
bultauzor <183804440+bultauzor@users.noreply.github.com>
2024-12-04 18:39:44 +0100

    feat(docker): Add kafka broker

---
+++ docker/kafka/kafka-start.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -m
+
+GREEN='\033[0;32m'
+RESET='\033[0m'
+
+/etc/kafka/docker/run&
+
+while ! /opt/kafka/bin/kafka-metadata-quorum.sh --bootstrap-controller localhost:9093 describe --status 2> /dev/null || false; do
+  sleep 0.2
+done
+
+echo -e "${GREEN}Kafka up${RESET}"
+
+/opt/kafka/bin/kafka-topics.sh --create --topic locations --bootstrap-server localhost:9092 --replication-factor 1 || true
+
+fg
