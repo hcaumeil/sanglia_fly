89160281be45d2f1dcedd65d66514889d9be7569
m-firmino <123177531+m-firmino@users.noreply.github.com>
2024-12-22 14:58:51 +0100

    shore(producer): format code

--- sanglia_fly_producer/producer.py
+++ sanglia_fly_producer/producer.py
@@ -19,46 +19,43 @@ print('nb live flight : ', len(live_flights))
 selected_flight = live_flights[0]
 
 
-
 def on_send_success(record_metadata):
     print('topic : ', record_metadata.topic)
     print('partition : ', record_metadata.partition)
     print('offset : ', record_metadata.offset)
 
+
 def on_send_error(excp):
-    print('I am an errback',excp)
-
-
+    print('I am an errback', excp)
 
 
 # Create a producer with JSON serializer
 producer = KafkaProducer(
-    client_id= '0001',
+    client_id='0001',
     bootstrap_servers='10.69.0.36:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8'),
     max_block_ms=3000,
 )
 while True:
-
-    data_flight = requests.get('https://data-live.flightradar24.com/clickhandler/?version=1.5&flight=' + selected_flight['id'])
+    data_flight = requests.get(
+        'https://data-live.flightradar24.com/clickhandler/?version=1.5&flight=' + selected_flight['id'])
     data_flight = data_flight.json()
 
     selected_data_flight = data_flight["trail"][0]
 
     data = {
-        'origin' : selected_flight['detail']['callsign'],
-        'latitude' : selected_data_flight['lat'],
-        'longitude' : selected_data_flight['lng'],
-        'altitude' : selected_data_flight['alt'] * 0.3048,
-        'orientation' : selected_data_flight['hd'],
-        'speed' : selected_data_flight['spd'],
-        'type' : selected_flight['detail']['ac_type'],
-        }
+        'origin': selected_flight['detail']['callsign'],
+        'latitude': selected_data_flight['lat'],
+        'longitude': selected_data_flight['lng'],
+        'altitude': selected_data_flight['alt'] * 0.3048,
+        'orientation': selected_data_flight['hd'],
+        'speed': selected_data_flight['spd'],
+        'type': selected_flight['detail']['ac_type'],
+    }
 
     print(data)
     # Sending JSON data
     producer.send(topic='locations', value=data).add_callback(on_send_success).add_errback(on_send_error)
 
-
     time.sleep(31)
-#producer.flush()
+# producer.flush()
