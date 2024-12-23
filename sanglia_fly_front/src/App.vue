55e5df72153b7638c89934e45b15cdaaf8a8d9dc
Clara Layus <layus.clara@gmail.com>
2024-12-23 15:39:55 +0100

    feat(front): init front part

---
+++ sanglia_fly_front/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <ViewInterface></ViewInterface>
+</template>
+
+<script>
+import ViewInterface from "@/components/ViewInterface.vue";
+
+export default {
+  name: "App",
+  components: {
+    ViewInterface,
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+</style>
+
