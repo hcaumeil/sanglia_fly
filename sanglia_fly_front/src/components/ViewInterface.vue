b9c659a085882daafc970b099cc8de731e195854
Siori9 <clara.layus@hotmail.fr>
2024-12-30 17:34:02 +0100

    fix(front): fix altitude

--- sanglia_fly_front/src/components/ViewInterface.vue
+++ sanglia_fly_front/src/components/ViewInterface.vue
@@ -95,9 +95,8 @@ export default {
         this.planeData = [...this.planeData];
       });
     },
-    normalizeAltitude(alt, altMin = 0, altMax = 12000) {
-      const normAlt = Math.min(Math.max((alt - altMin) / (altMax - altMin), 0), 1);
-      return (normAlt * 1000) / 12000000
+    normalizeAltitude(alt, altMin = 0, altMax = 200000) {
+      return Math.min(Math.max((alt - altMin) / (altMax - altMin), 0), 1);
     },
     onPlaneSelected(id) {
       this.selectedPlaneId = id;
